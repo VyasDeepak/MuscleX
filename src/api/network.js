e39b0aa76882a019778942622a8efa659d41f1f9
deepak vyas <magsysinfotech18@gmail.com>
2026-04-08 11:51:51 +0530

    WIP

--- src/api/network.js
+++
@@ -1,9 +0,0 @@
-import NetInfo from '@react-native-community/netinfo';
-  export const isNetworkAvailable = async () => {
-    return await NetInfo.fetch().then(state => {
-      console.log('NetInfo-------------',state.isConnected)
-      return state.isConnected
-    }).catch(error => {
-      return false
-    });
-  }
--- src/services/workoutService.js
+++
@@ -1,8 +0,0 @@
-import workouts from '../data/workouts.json';
-import exercises from '../data/exercises.json';
-
-export const getAllWorkouts = () => workouts;
-
-export const getWorkoutById = (id) => workouts.find(w => w.id === id);
-
-export const getExercisesByIds = (ids) => ids.map(id => exercises.find(e => e.id === id));
